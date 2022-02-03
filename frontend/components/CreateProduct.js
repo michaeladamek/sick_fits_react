f2db515d5943b6270e42f131f208927dfbfddd8f
Michael Adamek <michael.adamek@gmail.com>
2022-02-03 16:53:40 -0500

    Finished Module 5.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,7 +1,9 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import { Router } from 'next/router';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -40,9 +42,10 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
-  console.log(createProduct);
+  // console.log(createProduct);
   return (
     <Form
       onSubmit={async (e) => {
@@ -51,6 +54,11 @@ export default function CreateProduct() {
         // submit the inputfields to the backend
         const res = await createProduct();
         console.log(res);
+        clearForm();
+        // Go to that product's page
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
